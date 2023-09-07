f5e1fa4994e4d361b202718a7e2cbf841363b5f1
rafapaezbas <rpaezbas@rpaezbas.com>
2023-09-07 16:43:05 +0200

    added bee and autobase in different classes

--- test/index.test.js
+++ test/index.test.js
@@ -2,11 +2,15 @@ const Hypercore = require('hypercore')
 const Hyperbee = require('hyperbee')
 const ram = require('random-access-memory')
 const test = require('brittle')
-const Hypermetrics = require('../index.js')
+const { Hypermetrics, HypermetricsBee, HypermetricsAutobase } = require('../index.js')
 const Id = require('hypercore-id-encoding')
+const Corestore = require('corestore')
+const Autobase = require('@holepunchto/autobase')
 
 const client = require('prom-client')
 const metrics = new Hypermetrics(client)
+const beeMetrics = new HypermetricsBee(client)
+const autobaseMetrics = new HypermetricsAutobase(client)
 
 test('basic length metric', async (t) => {
   const core = new Hypercore(ram)
@@ -38,19 +42,40 @@ test('hyperbee metrics', async (t) => {
   await core.ready()
   await bee.ready()
 
-  metrics.addBee(bee)
+  beeMetrics.add(bee)
 
   await bee.put('key', 'value')
   await bee.del('key')
 
   await new Promise((resolve) => setTimeout(resolve, 100))
 
-  const result = await metrics.getMetricsAsJSON()
+  const result = await beeMetrics.getMetricsAsJSON()
   const putOperations = findMetric(result, Id.encode(core.key), 'hyperbee_put_operations')
   const delOperations = findMetric(result, Id.encode(core.key), 'hyperbee_del_operations')
 
   t.is(putOperations, 1)
   t.is(delOperations, 1)
+})
+
+test('autobase metrics', async (t) => {
+  const store = new Corestore(ram)
+  await store.ready()
+  const base = new Autobase(store, [], { apply, open })
+  await base.ready()
+
+  autobaseMetrics.add(base)
+
+  const result = await autobaseMetrics.getMetricsAsJSON()
+  const activeWriters = findMetric(result, base.local.id, 'autobase_active_writers')
+
+  t.is(activeWriters, 1)
+
+  function open (store) {
+    return store.get('test')
+  }
+
+  async function apply (nodes, view, base) {
+  }
 })
 
 function findMetric (metrics, key, name) {
