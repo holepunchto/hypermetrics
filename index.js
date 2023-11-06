22bd694246b5725de401b9bcea1322de2bfdf46c
rafapaezbas <rafa@holepunch.com>
2023-11-06 12:09:22 +0100

    added name label

--- index.js
+++ index.js
@@ -6,17 +6,21 @@ class Hypermetrics {
     this.uploadSpeedometers = new Map()
     this.downloadSpeedometers = new Map()
     this.cores = [] // TODO change to set
+    this.names = new Map()
 
     // for collect functions context
     const cores = this.cores
+    const names = this.names
 
     new this.client.Gauge({ // eslint-disable-line no-new
       name: 'hypercore_length',
       help: 'hypercore length',
-      labelNames: ['key', 'type'],
+      labelNames: ['key', 'type', 'name'],
       collect () {
         for (const core of cores) {
-          this.labels({ key: Id.encode(core.key), type: 'hypercore' }).set(core.length)
+          const key = Id.encode(core.key)
+          const name = names.get(key)
+          this.labels({ key, type: 'hypercore', name }).set(core.length)
         }
       }
     })
@@ -24,10 +28,12 @@ class Hypermetrics {
     new this.client.Gauge({ // eslint-disable-line no-new
       name: 'hypercore_indexed_length',
       help: 'hypercore indexed length',
-      labelNames: ['key', 'type'],
+      labelNames: ['key', 'type', 'name'],
       collect () {
         for (const core of cores) {
-          this.labels({ key: Id.encode(core.key), type: 'hypercore' }).set(core.indexedLength)
+          const key = Id.encode(core.key)
+          const name = names.get(key)
+          this.labels({ key, type: 'hypercore', name }).set(core.indexedLength)
         }
       }
     })
@@ -35,10 +41,12 @@ class Hypermetrics {
     new this.client.Gauge({ // eslint-disable-line no-new
       name: 'hypercore_contiguous_length',
       help: 'hypercore contiguous length',
-      labelNames: ['key', 'type'],
+      labelNames: ['key', 'type', 'name'],
       collect () {
         for (const core of cores) {
-          this.labels({ key: Id.encode(core.key), type: 'hypercore' }).set(core.contiguousLength)
+          const key = Id.encode(core.key)
+          const name = names.get(key)
+          this.labels({ key, type: 'hypercore', name }).set(core.contiguousLength)
         }
       }
     })
@@ -46,10 +54,12 @@ class Hypermetrics {
     new this.client.Gauge({ // eslint-disable-line no-new
       name: 'hypercore_byte_length',
       help: 'hypercore byte length',
-      labelNames: ['key', 'type'],
+      labelNames: ['key', 'type', 'name'],
       collect () {
         for (const core of cores) {
-          this.labels({ key: Id.encode(core.key), type: 'hypercore' }).set(core.byteLength)
+          const key = Id.encode(core.key)
+          const name = names.get(key)
+          this.labels({ key, type: 'hypercore', name }).set(core.byteLength)
         }
       }
     })
@@ -57,10 +67,12 @@ class Hypermetrics {
     new this.client.Gauge({ // eslint-disable-line no-new
       name: 'hypercore_contiguous_byte_length',
       help: 'hypercore contiguous byte length',
-      labelNames: ['key', 'type'],
+      labelNames: ['key', 'type', 'name'],
       collect () {
         for (const core of cores) {
-          this.labels({ key: Id.encode(core.key), type: 'hypercore' }).set(core.contiguousByteLength)
+          const key = Id.encode(core.key)
+          const name = names.get(key)
+          this.labels({ key, type: 'hypercore', name }).set(core.contiguousByteLength)
         }
       }
     })
@@ -68,10 +80,12 @@ class Hypermetrics {
     new this.client.Gauge({ // eslint-disable-line no-new
       name: 'hypercore_fork',
       help: 'hypercore fork',
-      labelNames: ['key', 'type'],
+      labelNames: ['key', 'type', 'name'],
       collect () {
         for (const core of cores) {
-          this.labels({ key: Id.encode(core.key), type: 'hypercore' }).set(core.fork)
+          const key = Id.encode(core.key)
+          const name = names.get(key)
+          this.labels({ key, type: 'hypercore', name }).set(core.fork)
         }
       }
     })
@@ -79,10 +93,12 @@ class Hypermetrics {
     new this.client.Gauge({ // eslint-disable-line no-new
       name: 'hypercore_nr_inflight_blocks',
       help: 'Total number of blocks in flight per core (summed across all peers)',
-      labelNames: ['key', 'type'],
+      labelNames: ['key', 'type', 'name'],
       collect () {
         for (const core of cores) {
-          this.labels({ key: Id.encode(core.key), type: 'hypercore' }).set(
+          const key = Id.encode(core.key)
+          const name = names.get(key)
+          this.labels({ key, type: 'hypercore', name }).set(
             core.peers.reduce((total, peer) => total + peer.inflight, 0)
           )
         }
@@ -92,10 +108,12 @@ class Hypermetrics {
     new this.client.Gauge({ // eslint-disable-line no-new
       name: 'hypercore_max_inflight_blocks',
       help: 'Max number of blocks in flight per core (summed across all peers)',
-      labelNames: ['key', 'type'],
+      labelNames: ['key', 'type', 'name'],
       collect () {
         for (const core of cores) {
-          this.labels({ key: Id.encode(core.key), type: 'hypercore' }).set(
+          const key = Id.encode(core.key)
+          const name = names.get(key)
+          this.labels({ key, type: 'hypercore', name }).set(
             core.peers.reduce((total, peer) => total + peer.getMaxInflight(), 0)
           )
         }
@@ -105,10 +123,12 @@ class Hypermetrics {
     new this.client.Gauge({ // eslint-disable-line no-new
       name: 'hypercore_peers',
       help: 'hypercore number of peers',
-      labelNames: ['key', 'type'],
+      labelNames: ['key', 'type', 'name'],
       collect () {
         for (const core of cores) {
-          this.labels({ key: Id.encode(core.key), type: 'hypercore' }).set(core.peers.length)
+          const key = Id.encode(core.key)
+          const name = names.get(key)
+          this.labels({ key, type: 'hypercore', name }).set(core.peers.length)
         }
       }
     })
@@ -116,32 +136,34 @@ class Hypermetrics {
     this.uploadedBlocks = new this.client.Counter({
       name: 'hypercore_uploaded_blocks',
       help: 'hypercore uploaded blocks',
-      labelNames: ['key', 'type']
+      labelNames: ['key', 'type', 'name']
     })
 
     this.downloadedBlocks = new this.client.Counter({
       name: 'hypercore_downloaded_blocks',
       help: 'hypercore downloaded blocks',
-      labelNames: ['key', 'type']
+      labelNames: ['key', 'type', 'name']
     })
 
     this.putOperations = new this.client.Counter({
       name: 'hyperbee_put_operations',
       help: 'hyperbee number of put operations',
-      labelNames: ['key', 'type']
+      labelNames: ['key', 'type', 'name']
     })
 
     this.delOperations = new this.client.Counter({
       name: 'hyperbee_del_operations',
       help: 'hyperbee number of del operations',
-      labelNames: ['key', 'type']
+      labelNames: ['key', 'type', 'name']
     })
   }
 
-  add (core) {
+  add (core, name) {
     this.cores.push(core)
-    core.on('upload', () => this.uploadedBlocks.labels({ key: Id.encode(core.key), type: 'hypercore' }).inc())
-    core.on('download', () => this.downloadedBlocks.labels({ key: Id.encode(core.key), type: 'hypercore' }).inc())
+    const key = Id.encode(core.key)
+    this.names.set(key, name)
+    core.on('upload', () => this.uploadedBlocks.labels({ key, type: 'hypercore', name }).inc())
+    core.on('download', () => this.downloadedBlocks.labels({ key, type: 'hypercore', name }).inc())
   }
 
   addBee (bee) {
--- test.js
+++ test.js
@@ -1,12 +1,16 @@
+const Hypercore = require('hypercore')
+const ram = require('random-access-memory')
 const test = require('brittle')
 const RAM = require('random-access-memory')
 const Corestore = require('corestore')
 const promClient = require('prom-client')
 const Hyperbee = require('hyperbee')
+const Id = require('hypercore-id-encoding')
 
 const HyperMetrics = require('./index')
 
 test('Returns expected metrics', async t => {
+  promClient.register.clear()
   const store = new Corestore(RAM)
   const core = store.get({ name: 'core' })
   await core.ready()
@@ -35,3 +39,80 @@ test('Returns expected metrics', async t => {
 
   t.alike(metricNames, expectedMetrics, 'Has all expected metrics')
 })
+
+test('basic length metric', async (t) => {
+  promClient.register.clear()
+  const metrics = new HyperMetrics(promClient)
+  const core = new Hypercore(ram)
+  await core.ready()
+
+  metrics.add(core)
+
+  await core.append(Date.now().toString())
+  await core.append(Date.now().toString())
+  await core.append(Date.now().toString())
+  await core.append(Date.now().toString())
+
+  const result = await metrics.getMetricsAsJSON()
+
+  const length = findMetric(result, Id.encode(core.key), 'hypercore_length')
+  const indexedLength = findMetric(result, Id.encode(core.key), 'hypercore_indexed_length')
+  const contiguousLength = findMetric(result, Id.encode(core.key), 'hypercore_contiguous_length')
+  const byteLength = findMetric(result, Id.encode(core.key), 'hypercore_byte_length')
+
+  t.is(length, 4)
+  t.is(indexedLength, 4)
+  t.is(contiguousLength, 4)
+  t.is(byteLength, 52)
+})
+
+test('adds core name as label', async (t) => {
+  promClient.register.clear()
+  const metrics = new HyperMetrics(promClient)
+  const core = new Hypercore(ram)
+  await core.ready()
+
+  const coreName = 'hypermetrics-test-core'
+  metrics.add(core, coreName)
+
+  await core.append(Date.now().toString())
+  await core.append(Date.now().toString())
+  await core.append(Date.now().toString())
+
+  const result = await metrics.getMetricsAsJSON()
+
+  const length = findMetricByCoreName(result, coreName, 'hypercore_length')
+
+  t.is(length, 3)
+})
+
+test('hyperbee metrics', async (t) => {
+  promClient.register.clear()
+  const metrics = new HyperMetrics(promClient)
+  const core = new Hypercore(ram)
+  const bee = new Hyperbee(core, { keyEncoding: 'utf-8', valueEncoding: 'utf-8' })
+  await core.ready()
+  await bee.ready()
+
+  metrics.addBee(bee)
+
+  await bee.put('key', 'value')
+  await bee.del('key')
+
+  await new Promise((resolve) => setTimeout(resolve, 100))
+
+  const result = await metrics.getMetricsAsJSON()
+  const putOperations = findMetric(result, Id.encode(core.key), 'hyperbee_put_operations')
+  const delOperations = findMetric(result, Id.encode(core.key), 'hyperbee_del_operations')
+
+  t.is(putOperations, 1)
+  t.is(delOperations, 1)
+})
+
+function findMetric (metrics, key, name) {
+  return metrics.find(e => e.name === name).values.find(e => e.labels.key === key).value
+}
+
+function findMetricByCoreName (metrics, coreName, name) {
+  return metrics.find(e => e.name === name).values.find(e => e.labels.name === coreName).value
+}
